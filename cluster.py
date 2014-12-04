eac012d16d75287995ec19408cb8636ba346c204
Clemens Drews <cdrews@us.ibm.com>
2014-12-04 08:22:54 -0800

    fixed clustered writeout

--- cluster.py
+++ cluster.py
@@ -3,6 +3,7 @@ import numpy as np
 from matplotlib import pyplot
 from sklearn import cluster
 from sklearn import preprocessing
+import json
 rows=[]
 r = csv.reader(open("results.csv"))
 r.next()
@@ -14,16 +15,18 @@ data=splt[0]
 titles=splt[1]
 data=np.array(data)
 data_scaled = preprocessing.scale(data.astype(float))
-k = 5
+k = 10
 kmeans = cluster.KMeans(n_clusters=k)
 kmeans.fit(data_scaled)
 
 labels = np.transpose(kmeans.labels_)
 print labels
-print np.shape(labels)
+s=np.shape(labels)
+labels=np.reshape(labels,(s[0],1))
 centroids = kmeans.cluster_centers_
 rows = np.hstack((data,titles))
-open("clustered.json","w").write(json.dumps(rows))
+rows = np.hstack((data,labels))
+open("clustered.json","w").write(json.dumps(rows.tolist()))
 
 
 for i in range(k):
